23b353db489cef91e8fbe3436dbbc319ab16dbcb
kerolos-raouf <83724226+kerolos-raouf@users.noreply.github.com>
2024-08-13 01:31:48 +0300

    finishing splash screen

---
+++ app/src/main/java/com/example/warmmeal/splashscreen/view/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.example.warmmeal.splashscreen.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.warmmeal.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+    }
+}
