4bfae9d826ce708ef2c9f3506865001d5a6658a4
kerolos-raouf <83724226+kerolos-raouf@users.noreply.github.com>
2024-08-15 15:11:40 +0300

    implementing meal screen

--- app/src/main/java/com/example/warmmeal/login_ways/view/LoginWays.java
+++ app/src/main/java/com/example/warmmeal/login_ways/view/LoginWays.java
@@ -1,11 +1,14 @@
 package com.example.warmmeal.login_ways.view;
 
 import android.content.Intent;
+import android.credentials.GetCredentialRequest;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,7 +19,18 @@ import com.example.warmmeal.model.util.ISkipAlertDialog;
 import com.example.warmmeal.model.util.Navigator;
 import com.example.warmmeal.model.util.SkipAlertDialog;
 import com.example.warmmeal.signup.view.SignUp;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginWays extends AppCompatActivity{
 
@@ -24,7 +38,7 @@ public class LoginWays extends AppCompatActivity{
     Button signUp,skip;
     TextView login;
 
-    //GoogleSignInClient mGoogleSignInClient;
+    GoogleSignInClient mGoogleSignInClient;
     private final int RC_SIGN = 20;
 
     @Override
@@ -82,25 +96,16 @@ public class LoginWays extends AppCompatActivity{
 
     void signInUsingGoogle()
     {
-        /*GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.gcm_defaultSenderId))
                 .requestEmail().build();
 
-        mGoogleSignInClient = GoogleSignIn.getClient(this,signInOptions);*/
+        mGoogleSignInClient = GoogleSignIn.getClient(this,signInOptions);
 
         gmail.setOnClickListener((e)->{
-            /*Intent intent = mGoogleSignInClient.getSignInIntent();
-            startActivityForResult(intent,RC_SIGN);*/
-            GetGoogleIdOption option = new GetGoogleIdOption.Builder()
-                    .setFilterByAuthorizedAccounts(false)
-                    .setServerClientId(getString(R.string.default_web_client_id))
-                    .build();
+            Intent intent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(intent,RC_SIGN);
 
-            /*if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-                android.service.credentials.GetCredentialRequest request = new GetCredentialRequest.Builder()
-                        .addCredentialOption(option)
-                        .build();
-            }*/
         });
 
     }
@@ -108,7 +113,7 @@ public class LoginWays extends AppCompatActivity{
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        /*if(requestCode == RC_SIGN)
+        if(requestCode == RC_SIGN)
         {
             Task<GoogleSignInAccount> accountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
@@ -118,10 +123,10 @@ public class LoginWays extends AppCompatActivity{
             {
                 Toast.makeText(this, "Error "+e.getMessage(), Toast.LENGTH_SHORT).show();
             }
-        }*/
+        }
     }
 
-    /*void fireBaseAuth(String token)
+    void fireBaseAuth(String token)
     {
         AuthCredential credential = GoogleAuthProvider.getCredential(token,null);
         FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -133,5 +138,5 @@ public class LoginWays extends AppCompatActivity{
                 }
             }
         });
-    }*/
+    }
 }
